17b01e00016204c38ac745265938133f57ca407b
Melissa Heying <melissakheying@gmail.com>
2020-05-01 18:47:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,27 @@
 function basicTeenager(age) {
-
+  if basicTeenager(13>=19) {
+    return `${You are a teenager}!` }
 }
 
 function teenager(age) {
+  if teenager(13>=19) {
+    return `You are a teenager!`
+  } else {
+    return `You are not a teenager`
+  }
+  }
+
 
 }
 
 function ageChecker(age) {
+  if ageChecker(13>=19) {
+    return `You are a teenager!`
+  } else if() {
+    return `You are a kid`
+  } else {
+    return `You are a grownup`
+  }
 
 }
 
